1e8ae82d827323c756bbc37e332b44c126c86f2d
Sidney Just <justsid@x-plane.com>
2023-12-01 14:28:38 -0800

    Added the ability to select a resolution for FPS tests

--- source/widgets/test_runner_dialog.cpp
+++ source/widgets/test_runner_dialog.cpp
@@ -44,6 +44,12 @@ test_runner_dialog::test_runner_dialog(xplane_installation *installation) :
 	m_settings_preset->addItem("3");
 	m_settings_preset->addItem("4");
 	m_settings_preset->addItem("5");
+
+	m_resolution_preset->addItem("Use current resolution");
+	m_resolution_preset->addItem("Fullscreen 1080p");
+	m_resolution_preset->addItem("Fullscreen 1440p");
+	m_resolution_preset->addItem("Window 1080p");
+	m_resolution_preset->addItem("Window 1440p");
 }
 
 uint32_t test_runner_dialog::get_fps_test() const
@@ -69,6 +75,27 @@ QStringList test_runner_dialog::get_arguments(const QString &telemetry_path) con
 
 	if(!telemetry_path.isEmpty())
 		result.push_back("--telemetry_path=" + xplaneify_path(telemetry_path) + "");
+
+	switch(m_resolution_preset->currentIndex())
+	{
+		case 0:
+		default:
+			break;
+
+		case 1:
+			result.push_back("--full=1920x1080");
+			break;
+		case 2:
+			result.push_back("--full=2560x1440");
+			break;
+
+		case 3:
+			result.push_back("--window=1920x1080");
+			break;
+		case 4:
+			result.push_back("--window=2560x1440");
+			break;
+	}
 
 	if(!m_additional_commands->text().isEmpty())
 		result.push_back(m_additional_commands->text());
